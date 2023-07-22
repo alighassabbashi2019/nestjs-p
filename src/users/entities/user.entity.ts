89aac40304b41c65fc43b97c369c9ea4efc915c1
alighassabbashi <alighassabbashidev@gmail.com>
2023-07-22 17:20:06 +0330

    add UserEdgeBuilder

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,6 @@
 import { IsString, Min } from 'class-validator';
 import { Product } from 'src/products/entities/product.entity';
+import { Edge, EdgeBuilderInterface } from 'src/types/edge.type';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
@@ -19,4 +20,31 @@ export class User {
 
   @OneToMany(() => Product, (product) => product.user)
   products: Product[];
+}
+
+export class UserEdgeBuilder
+  implements EdgeBuilderInterface<User, { id: string }>
+{
+  node: User;
+  cursor: string;
+
+  createCursor(data: { id: string }) {
+    return Buffer.from(JSON.stringify(data)).toString('base64');
+  }
+
+  readCursor(cursor: string) {
+    const stringifiedJson = Buffer.from(cursor, 'base64').toString('utf8');
+    return JSON.parse(stringifiedJson) as { id: string };
+  }
+
+  createEdge(nodes: User[]): Promise<Edge<User>[]> {
+    return Promise.all(
+      nodes.map((node) => {
+        const cursor = this.createCursor({
+          id: node.id,
+        });
+        return <Edge<User>>{ node, cursor };
+      }),
+    );
+  }
 }
